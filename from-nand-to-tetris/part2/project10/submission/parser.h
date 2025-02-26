9804f37b104c0a77d1cd96a1ac25c79c988e93e2
Gino Malpartida <gino.malpartida@pepsico.com>
2025-02-26 11:17:19 -0500

    create submission files

---
+++ from-nand-to-tetris/part2/project10/submission/parser.h
@@ -0,0 +1,33 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "lexer.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+typedef struct Parser
+{
+    PLEXER lexer;
+	char * out_filename;
+	FILE* fptr;
+} PARSER, *PPARSER;
+
+PPARSER parser_create(PLEXER lexer);
+void parser_destroy(PPARSER parser);
+void parser_print(PPARSER parser);
+void parser_print_ast(PPARSER parser);
+void parser_execute(PPARSER parser, char * out_filename);
+bool parser_keyword(PPARSER parser, char * keyword);
+bool parser_symbol(PPARSER parser, char * symbol);
+bool parser_identifier(PPARSER parser);
+bool parser_type(PPARSER parser);
+void parser_expression(PPARSER parser);
+void parser_statements(PPARSER parser);
+void parser_letStatement(PPARSER parser);
+void parser_whileStatement(PPARSER parser);
+void parser_doStatement(PPARSER parser);
+void parser_returnStatement(PPARSER parser); 
+void parser_subroutineCall(PPARSER parser);
+bool parser_isStatement(PPARSER parser);
+
+#endif
